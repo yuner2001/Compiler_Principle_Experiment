9971c82798c01b4b367e33bb39c19c08d7279d72
NoNameToChoose <3298520845@qq.com>
2022-03-17 21:02:51 +0800

    稍微写了写文档

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# GrammarClassifier
-编译原理: 一个简单的文法分析器
+# FoolFoolGrammarClassifier
+## 编译原理: 一个笨笨的文法分析器
+支持PSG,CSG,CFG,RLG四种文法的分类(含扩展CFG与扩展RLG，归于对应种类)
+使用时包含用头文件 [grammar_classifier](./src/grammar_classifier.cpp),调用其成员函数Classify即可，欢迎各位朋友一起完善。
+
+qiaqia 
--- main.cpp
+++ main.cpp
@@ -38,4 +38,5 @@ int main(int, char**) {
     String2Type("{{S,B,C,D},{a,b,c},P,S}{S->aSBC,S->aBC,CB->CD,CD->BD,BD->BC,aB->ab,bB->bb,bC->bc,cC->cc}");
     String2Type("{{Z,S,A,B,C},{a,b,c},P,Z}{Z->SC,S->aAc,A->aAc,A->bBb,C->aCb,C->?,B->bB,B->?}");
     String2Type("{{Z,U,V},{0,1},P,Z}{Z->U0,Z->V1,U->Z1,U->1,V->Z0,V->0}");
+    String2Type("{{S,A,B},{a,b},P,S}{S->aB,S->bA,A->a,A->aS,A->bAA,B->b,B->bS,B->aBB}");
 }
